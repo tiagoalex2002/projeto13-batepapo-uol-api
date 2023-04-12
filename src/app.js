7a943c43bdb2c43e5715e2de975f98cbcbed9c53
Tiago <tiagoalex0202@gmail.com>
2023-04-12 07:23:58 -0300

    Dotenv creation and configurations' setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
 
+//API's structure configurations
 const app= express()
 app.use(cors())
 app.use(express.json())
 app.listen(5000)
+dotenv.config()
 
+//Mongo Database configurantions and setup
 let db
-const mongoclient= new MongoClient("mongodb://localhost27017/dbUOL")
+const mongoclient= new MongoClient()
 mongoclient.connect().then(()=> db=mongoclient.db())
 mongoclient.connect().catch((er)=>console.log(er.message))
 
