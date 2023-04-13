e0f77a3ad210bc344336404922ef72adae59fab6
Tiago <tiagoalex0202@gmail.com>
2023-04-13 08:07:16 -0300

    .

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import dayjs from "dayjs"
 
 //API's structure configurations
 const app= express()
 app.use(cors())
 app.use(express.json())
-app.listen(5000)
+app.listen(5000, () => console.log("Servidor rodando"))
 dotenv.config()
 
 //Mongo Database configurantions and setup
@@ -18,7 +19,15 @@ mongoclient.connect().catch((er)=>console.log(er.message))
 
 
 
-app.post("/participants",(req,res))
+app.post("/participants",(req,res)=>{
+    const {name} = req.name;
+    let now= dayjs()
+    db.collection("participants").insertOne({name:name, lastStatus: Date.now()})
+    .then(participants => db.collection("messages").insertOne({from: name, to: 'Todos', text: 'entra na sala...', type:'status', time: now.format("HH:mm:ss")})
+    .then(messages => res.sendStatus(201))
+    .catch(err=>console.log(err.message)))
+    .catch(err=> console.log(err.message))
+})
 app.post("/messages",(req,res))
 app.post("/status",(req,res))
 
